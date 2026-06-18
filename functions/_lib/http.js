b7cff9593b1f438583242f0617f991133ae131c8
dlswjfal0o0 <4jtqjtsfmn@privaterelay.appleid.com>
2026-06-18 18:50:30 +0900

    Delete functions directory

--- functions/_lib/http.js
+++
@@ -1,9 +0,0 @@
-export function jsonResponse(obj, status = 200) {
-  return new Response(JSON.stringify(obj), {
-    status,
-    headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'Cache-Control': 'no-store'
-    }
-  });
-}
